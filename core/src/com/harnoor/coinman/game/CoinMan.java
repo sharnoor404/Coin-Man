b0abe6ea4293b3fcbce511d250ff4ee435ebda15
sharnoor404 <sharnoor404@gmail.com>
2020-06-02 13:43:16 +0530

    code

--- core/src/com/harnoor/coinman/game/CoinMan.java
+++ core/src/com/harnoor/coinman/game/CoinMan.java
@@ -8,29 +8,39 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+	Texture[] man ;
+	//man array stores array of images for our character
+	//texture is used to add images
 	@Override
 	//runs once when the game starts
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background=new Texture("bg.png");
+
 	}
 
 	@Override
 	//runs constantly
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	batch.begin();
+	batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+	    //0,0 marks the start of our background image(coordinate)
+        // we set the height and width to take up the whole screen
+        man=new Texture[4];
+        man[0]=new Texture("frame-1.png");
+        man[1]=new Texture("frame-2.png");
+        man[2]=new Texture("frame-3.png");
+        man[3]=new Texture("frame-4.png");
+
+
+
+	batch.end();
 	}
 	
 	@Override
 	//to start again, not used in our case
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
